5da1ea15ddbcc280cef34458ffed08bd4b3dff2f
pranjal verma <pranjal.verma150398@gmail.com>
2021-08-16 22:24:00 +0530

    Updated about me

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -61,9 +61,9 @@ const Timeline = () => {
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established
-        developers to take their development skills to the next level and build
-        awesome apps.
+        Hi, I am a javascript developer with a decent knowledge of its internal
+        working and an appetite for a lot more. I am self-taught and can adapt
+        to any library in reasonable time.
       </SectionText>
     </Section>
   );
